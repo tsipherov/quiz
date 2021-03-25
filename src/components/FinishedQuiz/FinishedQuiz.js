fe953fa983150e3296cf23d1514a6de5ab5f1e15
tsipherov <tsipherov@gmail.com>
2021-03-25 19:41:35 +0200

    added routing

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,48 +1,51 @@
-import React from 'react'
-import classes from './FinishedQuiz.module.scss'
-import Button from '../UI/Button/Button'
+import React from "react";
+import classes from "./FinishedQuiz.module.scss";
+import Button from "../UI/Button/Button";
+import { Link } from "react-router-dom";
 
-const FinishedQuiz = props => {
+const FinishedQuiz = (props) => {
   const successCount = Object.keys(props.results).reduce((total, key) => {
-    if (props.results[key] === 'success') {
-      total++
+    if (props.results[key] === "success") {
+      total++;
     }
 
-    return total
-  }, 0)
-
+    return total;
+  }, 0);
 
   return (
     <div className={classes.FinishedQuiz}>
       <ul>
-        { props.quiz.map((quizItem, index) => {
+        {props.quiz.map((quizItem, index) => {
           const cls = [
-            'fa',
-            props.results[quizItem.id] === 'error' ? 'fa-times' : 'fa-check',
-            classes[props.results[quizItem.id]]
-          ]
+            "fa",
+            props.results[quizItem.id] === "error" ? "fa-times" : "fa-check",
+            classes[props.results[quizItem.id]],
+          ];
 
           return (
-            <li
-              key={index}
-            >
+            <li key={index}>
               <strong>{index + 1}</strong>.&nbsp;
               {quizItem.question}
-              <i className={cls.join(' ')} />
+              <i className={cls.join(" ")} />
             </li>
-          )
-
-        }) }
+          );
+        })}
       </ul>
 
-      <p>Правильно {successCount} из {props.quiz.length}</p>
+      <p>
+        Правильно {successCount} из {props.quiz.length}
+      </p>
 
       <div>
-        <Button onClick={props.onRetry} type="primary">Повторить</Button>
-        <Button type="success">Перейти в список тестов</Button>
+        <Button onClick={props.onRetry} type="primary">
+          Повторить
+        </Button>
+        <Link to="/">
+          <Button type="success">Перейти в список тестов</Button>
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FinishedQuiz
+export default FinishedQuiz;
--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -1,41 +1,52 @@
-import React, {Component} from 'react'
-import classes from './Drawer.module.scss'
-import Backdrop from '../../UI/Backdrop/Backdrop'
+import React, { Component } from "react";
+import classes from "./Drawer.module.scss";
+import { NavLink } from "react-router-dom";
+import Backdrop from "../../UI/Backdrop/Backdrop";
 
 const links = [
-  1, 2, 3
-]
+  { to: "/", label: "Список", exact: true },
+  { to: "/auth", label: "Авторизация", exact: false },
+  { to: "/quiz-creator", label: "Создать тест", exact: false },
+];
 
 class Drawer extends Component {
+  clickHandler = () => {
+    this.props.onClose();
+  };
 
   renderLinks() {
     return links.map((link, index) => {
       return (
         <li key={index}>
-          <a>Link {link}</a>
+          <NavLink
+            to={link.to}
+            exact={link.exact}
+            activeClassName={classes.active}
+            onClick={this.clickHandler}
+          >
+            {link.label}
+          </NavLink>
         </li>
-      )
-    })
+      );
+    });
   }
 
   render() {
-    const cls = [classes.Drawer]
+    const cls = [classes.Drawer];
 
     if (!this.props.isOpen) {
-      cls.push(classes.close)
+      cls.push(classes.close);
     }
 
     return (
       <React.Fragment>
-        <nav className={cls.join(' ')}>
-          <ul>
-            { this.renderLinks() }
-          </ul>
+        <nav className={cls.join(" ")}>
+          <ul>{this.renderLinks()}</ul>
         </nav>
-        { this.props.isOpen ? <Backdrop onClick={this.props.onClose} /> : null }
+        {this.props.isOpen ? <Backdrop onClick={this.props.onClose} /> : null}
       </React.Fragment>
-    )
+    );
   }
 }
 
-export default Drawer
+export default Drawer;
