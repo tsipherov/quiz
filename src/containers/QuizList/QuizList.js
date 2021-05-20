8be7038db64d40e9e3cae800267e0f7c57d94f6c
tsipherov <tsipherov@gmail.com>
2021-05-20 20:38:56 +0300

    install axios

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import classes from "./QuizList.module.scss";
 import { NavLink } from "react-router-dom";
+import axios from "axios";
 
 export default class QuizList extends Component {
   renderQuizes() {
@@ -11,6 +12,14 @@ export default class QuizList extends Component {
         </li>
       );
     });
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://react-quiz-b9c12-default-rtdb.firebaseio.com/quiz.json")
+      .then((response) => {
+        console.log(response);
+      });
   }
 
   render() {
